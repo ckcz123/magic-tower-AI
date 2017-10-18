5e4224dff4a1db65ffb96cb384b5ff60d4e890e8
ckcz123 <ckcz123@126.com>
2017-10-18 16:40:02 +0800

    Update Map Generator

---
+++ map_generator/map_generator/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace map_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 416);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(560, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 416);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "加载图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "图片生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 452);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ map_generator/map_generator/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace map_generator
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<int, Bitmap> dict;
+        Dictionary<int, Color[,]> colorDict;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadResources();
+        }
+
+        private void loadResources()
+        {
+            dict = new Dictionary<int, Bitmap>();
+            Bitmap icon = Properties.Resources.icon;
+            Bitmap map = Properties.Resources.map;
+            Bitmap monster = Properties.Resources.monster;
+            Bitmap road = clipImage(map, 0, 0);
+
+            dict.Add(0, road);
+            dict.Add(1, clipImage(map, 32, 0));
+            dict.Add(81, clipImage(map, 0, 64, road));
+            dict.Add(82, clipImage(map, 32, 64, road));
+            dict.Add(83, clipImage(map, 64, 64, road));
+            dict.Add(87, clipImage(map, 32, 192, road));
+            dict.Add(88, clipImage(map, 0, 192, road));
+            dict.Add(40, clipImage(map, 32, 224, road));
+
+            dict.Add(11, clipImage(icon, 96, 32, road));
+            dict.Add(12, clipImage(icon, 64, 32, road));
+            dict.Add(13, clipImage(icon, 0, 64, road));
+            dict.Add(15, clipImage(icon, 0, 0, road));
+            dict.Add(16, clipImage(icon, 32, 0, road));
+            dict.Add(17, clipImage(icon, 64, 0, road));
+            dict.Add(18, clipImage(icon, 64, 64, road));
+            dict.Add(19, clipImage(icon, 96, 64, road));
+            dict.Add(20, clipImage(icon, 96, 96, road));
+            dict.Add(21, clipImage(icon, 96, 128, road));
+            dict.Add(22, clipImage(icon, 0, 32, road));
+            dict.Add(23, clipImage(icon, 32, 32, road));
+
+            for (int i = 0; i < 30; i++)
+            {
+                dict.Add(101 + i, clipImage(monster, 0, 32 * i, road));
+            }
+            dict.Add(199, clipImage(monster, 0, 36 * 32, road));
+
+
+            colorDict = new Dictionary<int, Color[,]>();
+            foreach (int id in dict.Keys)
+            {
+                Bitmap bitmap = dict[id];
+                Color[,] colors = new Color[32, 32];
+                for (int i = 0; i < 32; i++)
+                {
+                    for (int j = 0; j < 32; j++)
+                    {
+                        colors[i, j] = bitmap.GetPixel(i, j);
+                    }
+                }
+                colorDict.Add(id, colors);
+            }
+
+            SendMessage(textBox1.Handle, EM_SETTABSTOPS, 1, new int[] { 4 * 4 });
+
+        }
+
+        private const int EM_SETTABSTOPS = 0x00CB;
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr h, int msg, int wParam, int[] lParam);
+
+        private Bitmap clipImage(Bitmap source, int offsetX, int offsetY, Bitmap road = null)
+        {
+            Bitmap bitmap = new Bitmap(32, 32);
+            Graphics graphic = Graphics.FromImage(bitmap);
+            if (road != null)
+                graphic.DrawImage(road, 0, 0, new Rectangle(0, 0, 32, 32), GraphicsUnit.Pixel);
+            graphic.DrawImage(source, 0, 0, new Rectangle(offsetX, offsetY, 32, 32), GraphicsUnit.Pixel);
+            return bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(416, 416);
+            Graphics graphic = Graphics.FromImage(bitmap);
+            string[] lines = textBox1.Text.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Trim().Split('\t');
+                for (int j = 0; j < words.Length; j++)
+                {
+                    try
+                    {
+                        int id = Convert.ToInt32(words[j]);
+                        graphic.DrawImage(dict[id], new Rectangle(32 * j, 32 * i, 32, 32), new Rectangle(0, 0, 32, 32), GraphicsUnit.Pixel);
+                    }
+                    catch (Exception) { }
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void setText(Bitmap bitmap)
+        {
+            int width = bitmap.Width / 32, height = bitmap.Height / 32;
+            int score = 0;
+            string text = "";
+            int tmpScore = 0;
+            int[,] tmpMap = new int[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    // Bitmap temp = clipImage(bitmap, 32 * j, 32 * i);
+                    Color[,] colors = new Color[32, 32];
+                    for (int x = 0; x < 32; x++)
+                    {
+                        for (int y = 0; y < 32; y++)
+                        {
+                            colors[x, y] = bitmap.GetPixel(32 * j + x, 32 * i + y);
+                        }
+                    }
+                    int t = 0, v = 0;
+                    foreach (int id in colorDict.Keys)
+                    {
+                        // Bitmap value = dict[id];
+                        Color[,] value = colorDict[id];
+                        int s = 0;
+                        for (int x = 0; x < 32; x++)
+                        {
+                            for (int y = 0; y < 32; y++)
+                            {
+                                if (colors[x, y].Equals(value[x, y]))
+                                {
+                                    s++;
+                                }
+                            }
+                        }
+                        if (t < s)
+                        {
+                            t = s;
+                            v = id;
+                        }
+                    }
+                    tmpScore += t;
+                    tmpMap[i, j] = v;
+                }
+            }
+            if (score < tmpScore)
+            {
+                score = tmpScore;
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        // text += tmpMap[i, j];
+                        builder.Append(tmpMap[i, j]);
+                        if (j != height - 1) builder.Append('\t');
+                        else builder.Append("\r\n");
+                    }
+                }
+                text = builder.ToString();
+            }
+
+            textBox1.Text = text;
+            button2_Click(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image image = null;
+
+            if (Clipboard.ContainsImage())
+            {
+                image = Clipboard.GetImage();
+            }
+            else
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "图片文件|*.bmp;*.jpg;*.jpeg;*.gif;*.png";
+                fileDialog.FilterIndex = 1;
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    image = Image.FromFile(fileDialog.FileName);
+                }
+            }
+
+            if (image == null) return;
+            Bitmap bitmap = new Bitmap(image.Width, image.Height);
+            Graphics graphic = Graphics.FromImage(bitmap);
+            graphic.DrawImage(image, 0, 0);
+            // pictureBox1.Image = bitmap;
+            setText(bitmap);
+
+        }
+
+
+    }
+}
