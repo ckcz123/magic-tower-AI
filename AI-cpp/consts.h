9954c752ee2529c74402107e6d2bc959a40ab175
Zhang Chen <ckcz123@126.com>
2018-06-11 20:06:41 +0800

    Create consts.h

---
+++ AI-cpp/consts.h
@@ -0,0 +1,37 @@
+#ifndef AI_CONSTS_H
+#define AI_CONSTS_H
+
+#define ROAD 0
+#define WALL 1
+
+#define RED_JEWEL 27
+#define BLUE_JEWEL 28
+#define GREEN_JEWEL 29
+
+#define YELLOW_KEY 21
+#define BLUE_KEY 22
+#define RED_KEY 23
+#define GREEN_KEY 24
+
+#define RED_POTION 31
+#define BLUE_POTION 32
+#define GREEN_POTION 33
+#define YELLOW_POTION 34
+
+#define SWORD 35
+#define SHIELD 36
+
+#define SHOP 40
+
+#define YELLOW_DOOR 81
+#define BLUE_DOOR 82
+#define RED_DOOR 83
+#define GREEN_DOOR 84
+
+#define UP_STAIR 87
+#define DOWN_STAIR 88
+
+#define MONSTER_BOUND 201
+#define MONSTER_BOSS 299
+
+#endif //AI_CONSTS_H
